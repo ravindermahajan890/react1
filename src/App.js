1831ce01a6adf348635d9d7f267b45d3c328b2b6
ravinder mahajan <ravinder.mahajan@Ravinder-Mahajan.local>
2019-03-22 02:44:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with now deployment
+            Learn React with
           </a>
         </header>
       </div>
