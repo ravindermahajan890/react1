599982b8cccd03f327f8405d72b7df300039a11c
ravinder mahajan <ravinder.mahajan@Ravinder-Mahajan.local>
2019-03-22 02:50:56 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with
+            Learn React with now auto deployment
           </a>
         </header>
       </div>
